1a83495de977683cacf62c7432cd1aba02f7d85a
FLEX <WebAchieve@github.com>
2021-07-22 22:25:40 +0300

    upload

---
+++ precache-manifest.2b8fb8e5444810f4b0f1247982734e44.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6832b3ca03ff22ccc582",
+    "url": "/DragStore/css/app.b315bc2c.css"
+  },
+  {
+    "revision": "e1049db369c5ba966414",
+    "url": "/DragStore/css/chunk-6b018c3a.6acaed55.css"
+  },
+  {
+    "revision": "d45f8e175b2dcb898162",
+    "url": "/DragStore/css/chunk-9909dea2.b4f68631.css"
+  },
+  {
+    "revision": "4c17930570aa2b57b5d1cee90b13419c",
+    "url": "/DragStore/img/Group 1.svg"
+  },
+  {
+    "revision": "71efc7b62323e1ced07ddc7124465db1",
+    "url": "/DragStore/img/Group 2.svg"
+  },
+  {
+    "revision": "5a209950bbed9ede39cad9f476b96b8d",
+    "url": "/DragStore/img/Group 3.svg"
+  },
+  {
+    "revision": "83c01f76b1ae875b0f7c41730abcca5e",
+    "url": "/DragStore/img/User-1.jpg"
+  },
+  {
+    "revision": "ee685c2ca9d20b83037e055da3985f49",
+    "url": "/DragStore/img/User-2.jpg"
+  },
+  {
+    "revision": "f031f4655cd70e10af3dae57d5bc0e3b",
+    "url": "/DragStore/img/User-3.jpg"
+  },
+  {
+    "revision": "cf4e2d99b10bbdf8ac44f267a693a1ba",
+    "url": "/DragStore/img/User-4.jpg"
+  },
+  {
+    "revision": "65cd921aea4c5a4534e24eeecce97f31",
+    "url": "/DragStore/img/User-5.jpg"
+  },
+  {
+    "revision": "ebdeac6cb5c5e658f02a51d475177ee4",
+    "url": "/DragStore/img/Vector.png"
+  },
+  {
+    "revision": "17786b9e082033dcbe62d7663229482e",
+    "url": "/DragStore/img/bg.jpg"
+  },
+  {
+    "revision": "c19d8fbeeef6648919f44114074d7420",
+    "url": "/DragStore/img/result.jpg"
+  },
+  {
+    "revision": "2716f404e502e28cad71d5f8bfd304b5",
+    "url": "/DragStore/index.html"
+  },
+  {
+    "revision": "6832b3ca03ff22ccc582",
+    "url": "/DragStore/js/app.d5a33e77.js"
+  },
+  {
+    "revision": "e1049db369c5ba966414",
+    "url": "/DragStore/js/chunk-6b018c3a.22afee4e.js"
+  },
+  {
+    "revision": "d45f8e175b2dcb898162",
+    "url": "/DragStore/js/chunk-9909dea2.8916bc63.js"
+  },
+  {
+    "revision": "4dfd3947c013c5f05c8c",
+    "url": "/DragStore/js/chunk-vendors.54e9d1fd.js"
+  },
+  {
+    "revision": "aed233a56d441ae3415659514eabdde2",
+    "url": "/DragStore/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DragStore/robots.txt"
+  }
+]);
